b6c3603688100cf3c306a707242d3b0059424f6b
martonlanga <martonlanga@gmail.com>
2020-11-22 11:01:42 +0100

    Use markdown-to-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import Markdown from 'markdown-to-jsx';
 import React from 'react';
-import MDX from '@mdx-js/runtime';
-// Provide custom components for markdown elements
-const components = {
-  h1: (props) => <h1 style={{ color: 'tomato' }} {...props} />,
-  Demo: (props) => <h1>This is a demo component</h1>,
-};
-// Provide variables that might be referenced by JSX
-const scope = {
-  some: 'value',
-};
-const mdx = `
-# Hello, world!
-<Demo />
-<div>Here is the scope variable: {some}</div>
-`;
-export default () => (
-  <MDX components={components} scope={scope}>
-    {mdx}
-  </MDX>
+// surprise, it's a div instead!
+const MyParagraph = ({ children, ...props }) => (
+  <div style={{ color: props.color }}>{children}</div>
 );
+
+const md = `
+# DatePicker
+
+The DatePicker works by supplying a date to bias towards,
+as well as a default timezone.
+
+<MyParagraph color="green"><b>eyo</b></MyParagraph>
+
+
+`;
+
+const App = () => (
+  <Markdown
+    options={{
+      overrides: {
+        MyParagraph: {
+          component: MyParagraph,
+        },
+      },
+    }}
+  >
+    {md}
+  </Markdown>
+);
+
+export default App;
